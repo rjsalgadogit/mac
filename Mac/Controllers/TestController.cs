42f2f4bd69dc5fbd496f849b4a300f80520f7242
Ros Jason Salgado <ros-jason.salgado@besix.com>
2022-09-29 15:40:02 +0800

    Added test controller

---
+++ Mac/Controllers/TestController.cs
@@ -0,0 +1,13 @@
+﻿using Mac.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mac.Controllers
+{
+	public class TestController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
